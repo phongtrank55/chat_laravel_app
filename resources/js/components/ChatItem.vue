61100e8d2788183e7f8d61040979761f627661af
Bui Phong <phongtrank55@gmail.com>
2023-04-10 22:26:28 +0700

    first commit

---
+++ resources/js/components/ChatItem.vue
@@ -0,0 +1,34 @@
+<template>
+	<div class="message">
+		<div class="message-item user-name">
+			MTD
+		</div>
+		<div class="message-item timestamp">
+			| 11:21:32:
+		</div>
+		<div class="message-item text-message">
+			Hello how are you
+		</div>
+	</div>
+</template>
+
+<script>
+	export default {
+	}
+</script>
+
+<style lang="scss" scoped>
+	.message {
+		display: flex;
+		color: #00b8ff;
+		.message-item:not(:last-child) {
+			margin-right: 5px;
+		}
+	}
+	.message:not(:last-child) {
+		padding-bottom: 20px;
+	}
+	.is-current-user {
+		color: #a900ff;
+	}
+</style>
